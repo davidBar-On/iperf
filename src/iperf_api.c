4bc03315e132145b183a4bc4b64e6d8a17fe9b6e
DavidBar-On <david.cdb004@gmail.com>
2026-08-14 17:44:50 +0300

    Do not add JSON into worker_run context and set thread is running only if it was successful

--- src/iperf_api.c
+++ src/iperf_api.c
@@ -5034,6 +5034,7 @@ iperf_new_stream(struct iperf_test *test, int s, int sender)
         goto err_exit_close_buffer;
     }
     sp->pending_size = 0;
+    sp->thread_number = 0;
 
     /* Set socket */
     sp->socket = s;
--- src/iperf_error.c
+++ src/iperf_error.c
@@ -569,6 +569,10 @@ iperf_strerror(int int_errno)
         case IERVRSONLYSKIPRXCOPY:
             snprintf(errstr, len, "this OS does not support --skip-rx-copy");
             break;
+	case IEPTHREADNOTRUNNING:
+            snprintf(errstr, len, "a thread stopped running unexpectedly");
+            perr = 1;
+            break;
 	default:
             snprintf(errstr, len, "int_errno=%d", int_errno);
             perr = 1;
